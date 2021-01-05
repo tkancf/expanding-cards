3c29d7c1b2af88c92f924db01c7182479e042f09
tkancf <tkncf789@gmail.com>
2021-01-05 22:47:14 +0900

    Add images

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,11 @@
 <script>
+  let img1 = 'cooking_camp_bbq.png';
+  let img2 = 'food_gyouza_enban.png';
+  let img3 = 'food_spaghetti_vongole_bianco.png';
+  let img4 = 'food_sushi_kobore_ikura_don.png';
+  let img5 = 'syougatsu_mark_mochi.png';
 </script>
+
 
 <style>
 </style>
@@ -9,3 +15,9 @@
 </svelte:head>
 
 <h1>expanding-cards</h1>
+
+<img alt="img1" src={img1}>
+<img alt="img2" src={img2}>
+<img alt="img3" src={img3}>
+<img alt="img4" src={img4}>
+<img alt="img5" src={img5}>
